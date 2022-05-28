6a896739d0ac544027f97fef285f7256807ad40d
blackCapacitor <dhananjaykashyap@outlook.com>
2022-05-29 01:21:21 +0530

    search by id issue fixed

--- resources/views/web/search/search_by_id.blade.php
+++ resources/views/web/search/search_by_id.blade.php
@@ -1,36 +1,38 @@
-@if($errors->any())
-    <div role='alert' id='alert' class='alert alert-danger'>{{$errors->first()}}</div>
+@if ($errors->any())
+  <div role='alert' id='alert' class='alert alert-danger'>{{ $errors->first() }}</div>
 @endif
-{{--{!! Form::open(['url' => 'view_profile', 'class' => 'form-horizontal', 'id'=>'privacy_update']) !!}--}}
-<div class="row">
-    {!! Form::label('contact', 'Search By Profile Id', ['class' => 'col-sm-3 control-label']) !!}
+<form method="POST" action="{{ URL('view_profile') }}" class="form-horizontal" id='privacy_update'>
+  @csrf
+  <div class="row">
+    <label class="col-sm-3 control-label" id="contact">Search By Profile Id</label>
     <div class='col-sm-9'>
-        <div class='col-sm-1'>
-            {!! Form::label('mm', 'MM', ['class' => 'control-label']) !!}
-        </div>
-        <div class='col-sm-10'>
-            <input type="number" min="0" class="form-control input-sm numberOnly" id="profile_id"
-                   placeholder="Enter Profile Id">
-        </div>
+      <div class='col-sm-1'>
+        <label class="control-label" id="mm">MM</label>
+      </div>
+      <div class='col-sm-10'>
+        <input type="number" min="0" class="form-control input-sm numberOnly" id="profile_id"
+          placeholder="Enter Profile Id">
+      </div>
     </div>
     <div class='col-sm-offset-4 col-sm-9'>
-            <button type="button" onclick="search_by_profileid();" class="btn btn-sm btn-primary" style="margin-top: 10px;">Search</button>
+      <button type="button" onclick="search_by_profileid();" class="btn btn-sm btn-primary"
+        style="margin-top: 10px;">Search</button>
     </div>
-</div>
-{{--<p class="clearfix"></p>--}}
-{{--<div class="col-sm-12">--}}
-    {{--<div class='form-group'>--}}
+  </div>
+  <p class="clearfix"></p>
+  <div class="col-sm-12">
+    <div class='form-group'>
 
-    {{--</div>--}}
-{{--</div>--}}
-{{--{!! Form::close() !!}--}}
+    </div>
+  </div>
+</form>
 <script type="text/javascript">
-    function search_by_profileid() {
-        var profile_id = $('#profile_id').val();
-        if (profile_id == '') {
-            swal("Warning", "Please enter profile id", "info");
-        } else {
-            window.location.href = "{{url('view_profile').'/'}}" + profile_id;
-        }
+  function search_by_profileid() {
+    var profile_id = $('#profile_id').val();
+    if (profile_id == '') {
+      swal("Warning", "Please enter profile id", "info");
+    } else {
+      window.location.href = "{{ url('view_profile') . '/' }}" + profile_id;
     }
+  }
 </script>
